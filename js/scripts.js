761cfe3c42ffe3344334c561490801efcca959c1
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-01-28 14:55:05 -0300

    Update js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,38 @@
-var altura = 0
-var largura = 0
+// Ajustando as dimensões do palco do jogo
+var height = 0
+var width = 0
 
 function ajustaTamanhoPalcoJogo() {
-    altura = window.innerHeight
-    largura = window.innerWidth
+    width = window.innerWidth
+    height = window.innerHeight
 
-    console.log(altura, largura)
+    console.log(width, height)
 }
 
 ajustaTamanhoPalcoJogo()
+
+// Criação de posições randômicas
+
+var positionX = Math.floor(Math.random() * width) - 90
+var positionY = Math.floor(Math.random() * height) - 90
+
+// Ajuste caso a posição for menor que 0 e não der para ver o elemento randômico
+positionX = positionX < 0 ? 0 : positionX
+positionY = positionY < 0 ? 0 : positionY
+
+console.log(positionX, positionY)
+
+// Criação do elemento HTML, para utilizar as posições randômicas
+var mosquito = document.createElement('img')
+mosquito.src = './img/mosquito.png'
+mosquito.className = 'mosquito-1'
+
+// Atribuindo ao novo elemenento sua nova
+mosquito.style.left = positionX + 'px'
+mosquito.style.top = positionY + 'px'
+
+// Transforma a posição do elemento em abosluta, para que possa percorrer o body livremente, sem se basear nas suas próprias limitações
+mosquito.style.position = 'absolute'
+
+// Inserir no arquivo html o novo elemento
+document.body.appendChild(mosquito)
